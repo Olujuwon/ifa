dd253f2f2cef881f0d5c560de2383b986d361fee
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-01-04 14:40:19 +0200

    modified the looks and feel

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const members = ["Senad", 'Mohammed', 'Juwon', 'Sanjog', 'Sannu','Pragya','Anh', 'Raspin', 'Niloo', 'Lam'];
+const button =document.getElementById('d1butt');
+
 
 choosenOne= () =>{
     let choosen = Math.floor(Math.random()*10);
     console.log(choosen)
-    
-    return members[choosen]
+    return  document.getElementById("here").innerHTML = members[choosen]; 
 }
-const x =  choosenOne()
 
-document.getElementById("here").innerHTML = x;
+button.addEventListener('click', choosenOne)
+
+
